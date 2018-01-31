f85a771c04d4e8f18a61507d2f41a5f118a0aede
jerome <jerome.lorrain@protonmail.com>
2018-01-31 13:31:02 +0100

    First implementation of search by string return count, hashtags and places

--- server/httpd.py
+++ server/httpd.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+# -*- coding:  utf-8
 import socket
 import random
+import json
 import http.server
 import socketserver
 import urllib.parse
+from twitter_db import TwitterDB
 
 
 class HttpHandler(http.server.SimpleHTTPRequestHandler):
@@ -14,10 +17,21 @@ class HttpHandler(http.server.SimpleHTTPRequestHandler):
 
     def __init__(self, *args, **kwargs):
         self._session_id = None
+        self.db = TwitterDB()
         super().__init__(*args, **kwargs)
 
 
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
+    def send_JSON(self, json):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.encode(encoding='utf_8'))
+        return
 
     def do_GET(self):
         """Méthode à surcharger pour répondre à une requête HTTP get"""
@@ -29,22 +43,35 @@ class HttpHandler(http.server.SimpleHTTPRequestHandler):
         #Les paramètres de même noms sont fusionnés:
         #?p=v1&p=v2  devient { 'p': [ 'v1', 'v2' ]
         parameters = urllib.parse.parse_qs(parsed_url.query)
-        print ("Resource :")
-        print (parsed_url.path)
-        print ("Paramètres :")
-        print (parameters)
+        print ("Resource : '" + parsed_url.path+"'")
+        print ("Paramètres : "+str(parameters))
 
-        #L'appel à la méthode parente SimpleHTTPRequestHandler qui va renvoyer le fichier
-        #On n'est pas obligé de l'appeler.
-        super().do_GET()
+        content = ""
 
-
+        #Index
+        if parsed_url.path == "/":
+            super().do_GET()
+        #Search query
+        elif parsed_url.path == "/search":
+            print("Search Query")
+            if 'q' not in parameters:
+                print("No query parameter")
+                self.send_error(404, "No Query parameter (q) found")
+            else:
+                val = self.db.search(str(parameters['q'][0]))
+                if 'hashtags' in val:
+                    val['hashtags'] = dict(val['hashtags'])
+                if 'places' in val:
+                    val['places'] = dict(val['places'])
+                val = json.dumps(dict(val))
+                print("search done : "+str(val))
+                self.send_JSON(val)
 
 
 class ExtensibleHttpServer(socketserver.TCPServer):
     """Serveur HTTP qui étend celui donné par défaut. 2 Améilorations
        - on configure la socket pour pouvoir redémarrer immédiatement le serveur
-         si jamais on quitte le programme et on le relance (sinon il
+         si jamais on quitte le programme et on le relance (sinon   il
          faut attendre le timeout de la socket)
        - on ajoute une méthode serve_until_interrupted qui rattrape le CTRL-C dans le terminal.
 """
@@ -66,5 +93,5 @@ class ExtensibleHttpServer(socketserver.TCPServer):
 #Naviguer sur http://localhost:9010 après avoir lancé le serveur et observer la console
 
 if __name__ == "__main__":
-    HTTPD = ExtensibleHttpServer(("localhost", 9010), HttpHandler)
+    HTTPD = ExtensibleHttpServer(("127.0.0.1", 9010), HttpHandler)
     HTTPD.serve_until_interrupted()
---
+++ server/twitter_db.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+from pyspark.sql import SparkSession
+import json
+import pprint
+
+class TwitterDB():
+    def __init__(self, *args, **kwargs):
+        self.spark = SparkSession.builder \
+                        .appName("twitter_analyzer") \
+                        .config("spark.ui.showConsoleProgress","false") \
+                        .master("local").getOrCreate()
+
+        self.spark.sparkContext.setLogLevel("ERROR")
+
+    def test(self):
+        df = self.spark.read.json("hdfs://172.17.0.2:9000/input/twitter/tweets.json")
+        rdd = df.rdd
+        rdd = rdd.filter(lambda x: "test" in x['text']).map(lambda x: x['text'])
+        #
+
+        print(places.collect())
+
+        return rdd.first()
+
+    def search(self, query):
+        print('SEARCH QUERY : '+query)
+        #Creation d'une DATAFRAME (une « table ») à partir du fichier JSON se trouvant dans HDFS
+        #Il faut remplacer le chemin par le fichier sous hdfs
+        #on pourra le remplacer au début par un simple fichier local
+
+        df = self.spark.read.json("hdfs://172.17.0.2:9000/input/twitter/tweets.json")
+        rdd = df.rdd
+        res = {}
+        #Filter twitter text message containing query string
+        rdd = rdd.filter(lambda x: query in x['text'])
+
+        # Remove empty hashtags
+        hashtags = rdd.filter(lambda x: x['entities']['hashtags'] != [])
+        # Get the list of hashtags
+        hashtags = hashtags.flatMap(lambda x: x['entities']['hashtags'])
+        hashtags = hashtags.map(lambda x: (x['text'], 1))
+        # Reduce by key to count each hashtag
+        hashtags = hashtags.reduceByKey(lambda x, y: x + y)
+
+        #Remove unknown places
+        places = rdd.filter(lambda x: x['place'] != None)
+        #Get list of contry_code
+        places = places.map(lambda x: (x['place']['country_code'],1))
+        #Count number of tweet in each country_code
+        places = places.reduceByKey(lambda x,y: x+y)
+
+        res['count'] = rdd.count()
+        res['hashtags'] = hashtags.collect()
+        res['places'] = places.collect()
+
+        return res
+
+#Standelone Test
+if __name__ == "__main__":
+    t = TwitterDB()
+    s = t.test()
+    print(s)
